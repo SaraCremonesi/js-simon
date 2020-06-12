8b467a07b4d80c933df4c4a21f853c55d48ae392
SaraCremonesi <saracrem00@gmail.com>
2020-06-12 17:04:22 +0200

    completamento esercizio

--- script.js
+++ script.js
@@ -13,12 +13,30 @@ while (i < 5) {
     numeroProibito = true;
   } else {
     arrayNumeriCasuali.push(randomInteger);
-    i++
+    i++;
   }
 };
   alert('I numeri da ricordare sono: ' + arrayNumeriCasuali);
 
- var time = setTimeout(timeOut,3000);
+// Setto la funzione di setTimeout per far apprire dopo 30 secondi dalla
+ // chiusura dell'alert i 5 prompt per l'iserimento dei numeri memorizzati
+// (utilizzo una funzione anonima come primo argomento del setTimeout,
+ // ma avrei potuto anche creare una funzione a parte da invocare nell'argomento)
+ // poi sempre all'interno della funzione eseguo le operazioni di calcolo del punteggio,
+ // ovvero utilizzo la funzione .includes() in un ciclo if per controllare se
+ // il numero inserito sia esatto o sbagliato, se è esatto lo pusho in un array e
+ // il punteggio totalizzato sarà dato dalla lunghezza stessa dell'array
+ setTimeout(function() {
+   var arrayNumeriIndovinati = [];
+     for (i = 0; i < 5; i++) {
+       var numeroInserito = parseInt(prompt('Inserisci uno dei 5 numeri che ti ricordi'));
+       if (arrayNumeriCasuali.includes(numeroInserito)) {
+         arrayNumeriIndovinati.push(numeroInserito);
+       }
+     };
+     console.log('Hai totalizzato ' + arrayNumeriIndovinati.length + ' punti su 5');
+   },30000);
+
 
 // ****************FUNZIONI****************
 // Funzione per evitare le ripetizioni degli stessi numeri casuali
@@ -31,10 +49,3 @@ function verificaPresenza(numero, array) {
   };
   return presenza;
 };
-
-// Funzione per far partire i 30 secondi
-function timeOut() {
-    for (i = 0; i < 5; i++) {
-      var numeroInserito = parseInt(prompt('Inserisci uno dei 5 numeri che ti ricordi'));
-    }
-  };
